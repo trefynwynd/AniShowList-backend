146550b2804d64c05a37e10d53cf3d05404734e3
trefynwynd <jl.alejandria@gmail.com>
2020-11-17 16:09:19 -0800

    Model creation complete.

---
+++ models/userwatch.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class userWatch extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  userWatch.init({
+    userId: DataTypes.INTEGER,
+    apiId: DataTypes.INTEGER,
+    watching: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'userWatch',
+  });
+  return userWatch;
+};
