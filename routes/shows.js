c1bf2cb7e27d34fc3b076527dfcd9ae3b6d06f7f
trefynwynd <jl.alejandria@gmail.com>
2020-12-01 16:38:20 -0800

    Updated the routes.

--- routes/shows.js
+++ routes/shows.js
@@ -3,10 +3,10 @@ const router = require('express').Router();
 const ctrl = require('../controllers');
 
 // routes
-router.get('/', ctrl.shows.watchingList);
+router.get('/:userId', ctrl.shows.watchingList);
 router.post('/', ctrl.shows.watchStatus);
 // This is based on the delete method in routes/userFavorites
-router.put('/:userId/:watching', ctrl.shows.statusChange);
+router.put('/:userId', ctrl.shows.statusChange);
 
 // exports
 module.exports = router;
