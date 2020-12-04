d63fcb8b5da305a0b7ee221e670cc0bc9b6451f0
trefynwynd <jl.alejandria@gmail.com>
2020-12-03 22:15:01 -0800

    removed unneeded files.

--- routes/favorites.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const controllers = ('../controllers/shows')
-
-router.get('/', function(req, res) {
-    res.redirect('/favorites')
-})
-
-module.exports = router
